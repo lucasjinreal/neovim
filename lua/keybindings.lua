9c17757a1e1e0a21c647b931890ab45d6d0ae2e8
Your Name <you@example.com>
2022-09-28 20:29:42 +0800

    add

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,4 +1,12 @@
+local map = vim.api.nvim_set_keymap
+local opt = {noremap = true, silent = true }
+
 -- nvimTree
 map('n', '<A-m>', ':NvimTreeToggle<CR>', opt)
 
 map('n', '<C-n>', ':NvimTreeToggle', opt)
+
+map("n", "<S-v>", ":vsp<CR>", opt)
+map("n", "<S-h>", ":sp<CR>", opt)
+map("n", "<S-c>", "<C-w>c", opt)
+map("n", "<S-a>", "<C-w>o", opt) -- close others
