023cef9be8756753e686cc02016e33e2af390920
Your Name <you@example.com>
2022-09-28 20:17:09 +0800

    add

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,2 +1,4 @@
 -- nvimTree
 map('n', '<A-m>', ':NvimTreeToggle<CR>', opt)
+
+map('n', '<C-n>', ':NvimTreeToggle', opt)
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -14,4 +14,16 @@ return require('packer').startup(function()
         'kyazdani42/nvim-tree.lua',
         requires = 'kyazdani42/nvim-web-devicons'
     }
+    
+    -- for which key
+    use {
+        "folke/which-key.nvim",
+        config = function()
+        require("which-key").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  
+}
 end)
