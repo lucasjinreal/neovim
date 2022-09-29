70017f751805f26cc9b432f6b8fa33c734328b16
Your Name <you@example.com>
2022-09-30 00:25:07 +0800

    ctrl + n open tree

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -2,7 +2,7 @@ local map = vim.api.nvim_set_keymap
 local opt = {noremap = true, silent = true }
 
 -- nvimTree
-map('n', '<A-m>', ':NvimTreeToggle<CR>', opt)
+map('n', '<C-n>', ':NvimTreeToggle<CR>', opt)
 
 map("n", "s", "", opt)
 -- windows 分屏快捷键
