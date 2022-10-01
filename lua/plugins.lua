d9e92dc360bc50ecfa4e89685f25e3c82bf119de
Your Name <you@example.com>
2022-10-01 16:05:15 +0800

    remove which key

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -14,15 +14,8 @@ local packer_bootstrap = ensure_packer()
 return require('packer').startup(function()
     use 'wbthomason/packer.nvim'
 
-    -- gruvbox theme
-    use {
-        "ellisonleao/gruvbox.nvim",
-        requires = {"rktjmp/lush.nvim"}
-    }
-
     use 'glepnir/zephyr-nvim'
     use("folke/tokyonight.nvim")
-
     use 'bluz71/vim-moonfly-colors'
 
     -- nvim-tree (新增)
@@ -31,17 +24,6 @@ return require('packer').startup(function()
         requires = 'kyazdani42/nvim-web-devicons'
     }
     
-    -- for which key
-    use {
-        "folke/which-key.nvim",
-        config = function()
-          require("which-key").setup {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-          }
-        end
-    }
-  
+    use { "beauwilliams/focus.nvim", config = function() require("focus").setup() end }
 
 end)
